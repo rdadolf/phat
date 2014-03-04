4fa8921940a21c25e35da500fa4db412d3922d33
Bryan Kate <bkate@eecs.harvard.edu>
2014-03-04 02:50:29 -0500

    rpcs

---
+++ phat-rpc.hh
@@ -0,0 +1,16 @@
+#ifndef PHAT_RPC_HH
+#define PHAT_RPC_HH
+
+namespace phat {
+
+    enum {
+        phat_keepalive = 0;
+    };
+
+    enum {
+        phat_ok = 0,
+        phat_fail = -1
+    };
+}
+
+#endif
